4844c73de49140f227f549d5b82badae6a9a4214
mandira <mandirachl@gmail.com>
2022-04-25 18:14:35 +0530

    updates

--- src/news-update-admin/newsUpdateAdmin.schema.ts
+++ src/news-update-admin/newsUpdateAdmin.schema.ts
@@ -1,10 +1,13 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document, SchemaTypes } from 'mongoose';
+import { Board } from 'src/board/board.schema';
+import { User } from 'src/user/user.schema';
+
 
 export type NewsDocument = News & Document;
 
 @Schema()
-export class News{
+export class News {
     @Prop()
     image: string;
     
--- src/testimonial/testimonial.schema.ts
+++ src/testimonial/testimonial.schema.ts
@@ -17,5 +17,4 @@ export class Testimonial {
   @Prop()
   customerReview: string;
 }
-
 export const TestimonialSchema = SchemaFactory.createForClass(Testimonial);
