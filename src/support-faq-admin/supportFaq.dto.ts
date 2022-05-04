0542458041a091b10a852c960c219320a9207120
mandira <mandirachl@gmail.com>
2022-05-04 17:16:46 +0530

    updates

--- src/support-faq-admin/supportFaq.dto.ts
+++ src/support-faq-admin/supportFaq.dto.ts
@@ -2,11 +2,11 @@ import { PartialType } from '@nestjs/mapped-types';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class SupportFaqCreateDto {
-  @IsString()
+
   @IsNotEmpty()
   questionFaq: string;
 
-  @IsString()
+ 
   @IsNotEmpty()
   answerFaq: string;
 }
