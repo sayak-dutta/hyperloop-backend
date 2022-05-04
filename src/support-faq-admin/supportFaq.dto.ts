97b2cf5fec2928289f077927d5df5623e05bda2a
Sayak Dutta <dsayak29@gmail.com>
2022-05-04 19:33:02 +0530

    Imported bannerModule into app module

--- src/support-faq-admin/supportFaq.dto.ts
+++ src/support-faq-admin/supportFaq.dto.ts
@@ -2,11 +2,9 @@ import { PartialType } from '@nestjs/mapped-types';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class SupportFaqCreateDto {
-
   @IsNotEmpty()
   questionFaq: string;
 
- 
   @IsNotEmpty()
   answerFaq: string;
 }
